2cd2ea2a966915c71e456dd7f4cb2a0f14d3bfa4
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2023-12-28 11:07:24 +0530

    buttons

--- src/components/EventCardComponent.jsx
+++ src/components/EventCardComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function EventCard() {
   return (
-    <div className=" px-8">
+    <div className=" px-8 py-6">
      <div class="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
     <img
       src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
--- src/components/EventsPage.jsx
+++ src/components/EventsPage.jsx
@@ -1,41 +1,37 @@
 import React from "react";
 import EventCard from "./EventCardComponent";
-import Vanta from "./Vanta";
+
 
 function EventsPage() {
   return (
     <div>
-      <div>
-        <div class="mx-auto px-8 ">
-          <div class="mt-10 hidden w-full flex-col justify-between space-y-4 md:flex md:flex-row">
-            <div class="flex w-full items-end border-b border-gray-300">
-              <div class="cursor-pointer px-6 py-1 leading-normal  ">
-                <button
-                  type="button"
-                  class="inline-flex items-center rounded-md bg-gray-800 px-7 py-2 text-sm font-semibold text-white hover:bg-black/80"
-                >All</button>
-              </div>
-              <div class="cursor-pointer px-6 py-1 leading-normal  ">
-              <button
-                  type="button"
-                  class="inline-flex items-center rounded-md bg-black px-6 py-2 text-sm font-semibold text-white hover:bg-black/80"
-                >Clubs</button>
-              </div>
-              <div class="cursor-pointer px-6 py-1 leading-normal ">
-              <button
-                  type="button"
-                  class="inline-flex items-center rounded-md bg-black px-4 py-2 text-sm font-semibold text-white hover:bg-black/80"
-                >Department</button>
-              </div>
-            </div>
-          </div>
-          <div class="mx-auto grid w-full  items-center space-y-8 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-3">
-          <EventCard/>
-          <EventCard/>
-          <EventCard/>
-          </div>
+        <div>
+            
+
+<ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+    <li class="me-2">
+        <span  className="inline-block p-4  hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Profile</span>
+    </li>
+    <li class="me-2">
+        <span  className="inline-block p-4  hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</span>
+    </li>
+    <li class="me-2">
+        <span  className="inline-block p-4  hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</span>
+    </li>
+    <li class="me-2">
+        <span  className="inline-block p-4  hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</span>
+    </li>
+</ul>
+
+            
         </div>
-    
+        
+      <div className=" grid w-full  items-center space-y-4 px-5 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-3">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
       </div>
     </div>
   );
